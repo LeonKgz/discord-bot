5bc156ac948464d359119d1382a142f065d5db9e
LeonKgz <leonkhalmetov@gmail.com>
2022-01-17 16:50:04 +0000

    Added a file function to give a summary of discord server users.

--- main.py
+++ main.py
@@ -336,6 +336,242 @@ async def parse_zettel_json(ctx, data):
       f.close()
       os.remove(filename)
 
+@bot.command(name="file")
+async def ebmed(ctx, user):
+
+  id_to_search = get_id(user)
+  mem = bot.get_user(id_to_search)
+
+  embed = discord.Embed(title=f"Досье") 
+  embed.set_author(name=mem.display_name, icon_url=mem.avatar_url)
+
+  #description = get_description(id_to_search)
+  row = get_db_row("confessions", id_to_search)
+  
+  mean_str = "⠀\n"
+
+  if row:
+    #points = row["Points"]
+    description = row["Confession"]
+
+    if (row["Points"] is None):
+      data = {}
+      
+    else:
+      data = json.loads(row["Points"])
+      #data[f"{id_author}"] = points
+
+    vals = list(data.values())
+
+    if len(vals) == 0:
+      curr_mean = False
+      mean_str = "⠀\nОписание пока не было оценено\n\n"
+    else:
+      curr_mean = int(np.mean(vals))
+      mean_str = f"⠀\nСредняя оценка описания — *{curr_mean} / 10*\n\n"
+      # TODO in the future we can also check the respective entry in the unmarked_confessions to make it clear
+      # whether the current mark is final or there are still moderators who have yet to mark it
+
+  else:
+    #mean_str = f"Средняя оценка описания — *{curr_mean} / 10*"
+    description = False
+    print("here")
+
+  if not description:
+    description = f"{mem.display_name} пока не предоставил(а) своё описание."
+
+  if (len(description) > 1024):
+    description = description[:1019] + "..."
+
+  embed.add_field(name="⠀", value=f"*{description}*", inline=False)
+
+
+
+
+  thumbs = {
+    "zek": "https://i.ibb.co/fqsc4TP/image.jpg",
+    "shiz": "https://i.ibb.co/Vvsc71q/shiz.jpg",
+    "polit_buro": "https://i.ibb.co/JBr8jVj/image.png",
+    "sovnarmod": "https://i.ibb.co/mR5ZxHK/zacon.jpg",
+    "vchk": "https://i.ibb.co/KXDLLsr/vchk.jpg",
+    "truppa": "https://i.ibb.co/2KCYPQZ/hands-hold-a-theater-mask-dot-technique-of-drawing-change-of-role-in-ather-Theater-Actor-tattoo-Hypo.jpg",
+    "narod_artist": "https://i.ibb.co/6mbXrJq/narod-artist-tmg.png",
+    "blatnoi": "https://i.ibb.co/s1Q7gwc/blatnoi.jpg",
+    "onizuka": "https://i.ibb.co/wwJPFYR/onizuka.jpg",
+    "chtets": "https://i.ibb.co/5K5hGjH/chtets.jpg",
+    "actor_zapasa": "https://i.ibb.co/vPk10R5/actor-zapasa.jpg",
+    "proletariat": "https://i.ibb.co/VQtxxPj/proletariat.jpg",
+    "apatrid": "https://i.ibb.co/DwsbRGY/apatrid.jpg",
+  }
+ 
+  colours = {
+    "zek": 0x9b59b6,
+    "shiz": 0xe67e22,
+    "polit_buro": 0xf30101,
+    "sovnarmod": 0x2ecc71,
+    "vchk": 0xf1c40f,
+    "truppa": 0x1abc9c,
+    "narod_artist": 0x3498db,
+    "blatnoi": 0xe91e63,
+    "onizuka": 0xe67e22,
+    "chtets": 0xe67e22,
+    "actor_zapasa": 0x1abc9c,
+    "proletariat": 0x99aab5,
+    "apatrid": 0x2f3136,
+  }
+  
+  
+  #embed = discord.Embed(title=f"Досье", description=f"", color=0xa87f32, url="https://albenz.xyz", footer="Переводчик — модолец!") #creates embed
+  #embed.set_author(name=mem.display_name, url="https://twitter.com/RealDrewData", icon_url=mem.avatar_url)
+
+  # thumbnail has an icon for different roles, i.e. Proletari, SovNarMod
+  #embed.set_thumbnail(url=thumb)
+  
+  roles_names_list = [
+    "Апатрид",
+    "Политзаключённый",
+    "Шиз",
+    "Политбюро ЦКТМГ",
+    "СовНарМод",
+    "ВЧК",
+    "Драматическая Труппа",
+    "Народный Артист ТМГ",
+    "Блатной",
+    "Востоковед",
+    "Чтец",
+    "Актёр Запаса",
+    "Пролетарий",
+  ]
+  
+  roles_names_to_code = {
+    "Политзаключённый": "zek",
+    "Шиз": "shiz",
+    "Политбюро ЦКТМГ": "polit_buro",
+    "СовНарМод": "sovnarmod",
+    "ВЧК": "vchk",
+    "Драматическая Труппа": "truppa",
+    "Народный Артист ТМГ": "narod_artist",
+    "Блатной": "blatnoi",
+    "Востоковед": "onizuka",
+    "Чтец": "chtets",
+    "Актёр Запаса": "actor_zapasa",
+    "Пролетарий": "proletariat",
+    "Апатрид": "apatrid",
+  }
+
+
+  guild = bot.get_guild(GUILD) 
+  for member in guild.members:
+    if (member.id == id_to_search):
+      mem_roles = list(map(str, member.roles))
+      for role in roles_names_list:
+        if role in mem_roles:
+          embed.set_thumbnail(url=thumbs[roles_names_to_code[role]])
+          embed.color = colours[roles_names_to_code[role]]
+
+          dominating_role = role
+          #embed.set_footer(text="СовНарМод ТМГ")
+          embed.set_footer(text=dominating_role)
+          
+          break
+
+  # SET @row_number = 0; 
+  # SELECT num, ID, NAME, Points 
+  # FROM (SELECT (@row_number:=@row_number + 1) AS num, ID, Name, Points 
+  # FROM raiting ORDER BY Points DESC) a
+  # WHERE ID = '696405991876722718'
+
+  db, cursor = get_db_cursor()
+  sql = f"SET @row_number = 0; SELECT (@row_number:=@row_number + 1) AS num, ID, Name, Points FROM raiting ORDER BY Points DESC"
+
+  try:
+    #cursor.execute(sql)
+    cursor.execute("SET @row_number = 0;")
+    cursor.execute(f"SELECT num, ID, Name, Points FROM (SELECT (@row_number:=@row_number + 1) AS num, ID, Name, Points FROM raiting ORDER BY Points DESC) a WHERE ID = {id_to_search}")
+
+    res = cursor.fetchone()
+    num = res["num"]
+    points = res["Points"]
+    #db.commit()
+  except Exception as e:
+    print(e)
+    db.rollback()
+  db.close()
+
+
+	# Counting the number of tellings on the target member
+  counter = 0
+  
+  db, cursor = get_db_cursor()
+  try:
+    sql = f"SELECT COUNT(*) FROM tellings WHERE Target = {id_to_search}"
+    cursor.execute(sql)
+    counter = int(cursor.fetchone()['COUNT(*)'])
+  except Exception as e:
+    print(e)
+    db.rollback()
+  
+  db.close()
+  #mod = counter % 10
+  #counter_str = "раз"
+  #if mod > 1 and mod < 5:
+  #  counter_str = "раза"
+
+  # get the number of times a person has been imprisoned
+  imprisoned = 0
+  row = get_db_row("prisoners", id_to_search)
+  if row:
+    imprisoned = int(row["Counter"])
+    
+  main_field = f"Социальный Рейтинг — *{points} ( {num}-е место )*\n\nНа гражданина донесено *{counter} {get_times_str(counter)}*\n\nЗаключён в ГУЛАГ *{imprisoned} {get_times_str(imprisoned)}*" 
+
+  if description:
+    main_field = mean_str + main_field
+  else: 
+    main_field = "⠀\n" + main_field
+
+  embed.add_field(name=main_field, value="⠀", inline=False)
+
+  # attach moshna печать
+#  embed.add_field(name="Валюта", value="Шанырак - 12", inline=True)
+  await ctx.send(embed=embed)
+
+# depending on the number returns the currect russian analogue of «times» i.e. раза/раза
+def get_times_str(num):
+  mod = num % 10
+  counter_str = "раз"
+  
+  if mod > 1 and mod < 5:
+    counter_str = "раза"
+
+  return counter_str
+
+
+
+@bot.command(name="манифест")
+async def website(ctx):
+  await ctx.send(f"<@!{ctx.author.id}>, тебе сюда => https://albenz.xyz/files/tractatus.pdf")
+
+@bot.command(name="сайт")
+async def website(ctx):
+  await ctx.send(f"<@!{ctx.author.id}>, тебе сюда => https://albenz.xyz")
+
+@bot.command(name="пьесы")
+async def website(ctx):
+  await ctx.send(f"<@!{ctx.author.id}>, тебе сюда => https://albenz.xyz/plays/allplays/")
+
+@bot.command(name="песни")
+async def website(ctx):
+  await ctx.send(f"<@!{ctx.author.id}>, тебе сюда => https://www.albenz.xyz/songs/allartists/")
+
+@bot.command(name="plays")
+async def website(ctx):
+  await ctx.send(f"<@!{ctx.author.id}>, тебе сюда => https://www.youtube.com/channel/UCdVx_oiTYB8fQdkbYmUPRQQ")
+
+@bot.command(name="songs")
+async def website(ctx):
+  await ctx.send(f"<@!{ctx.author.id}>, тебе сюда => https://www.youtube.com/channel/UCVRzrqkQxWawb-cKz1zXBZQ")
+
 @bot.command(name="долг")
 async def duty(ctx, issue):
   url = f"http://albenz.xyz:6969/duty?issue={issue}"
@@ -536,28 +772,45 @@ async def jail(ctx, poor_guy, protocol):
       words = {"zamechanie": "10 минут", "vigovor": "1 час", "zakluchenie": "сутки"}
 
       mem_id = mem.id
-      sql = f"SELECT * from prisoners WHERE ID={mem_id};"
+      
+      row = get_db_row("prisoners", mem_id)
+      
+      if row:
 
-      try:
-        cursor.execute(sql)
-        res = cursor.fetchone()['Status']
+        # New value to update the entry row 
+        imprisonment_cnt = row['Counter'] + 1
+
+        res = row['Status']
         newidx = statuses.index(res) + 1
         if (newidx == len(statuses)):
           newidx = 0
         status = statuses[newidx]
-        
-      except Exception as e:
+      else:
         status = statuses[0]
+        imprisonment_cnt = 1 
+
+      #sql = f"SELECT * from prisoners WHERE ID={mem_id};"
+
+      #try:
+      #  cursor.execute(sql)
+      #  res = cursor.fetchone()['Status']
+      #  newidx = statuses.index(res) + 1
+      #  if (newidx == len(statuses)):
+      #    newidx = 0
+      #  status = statuses[newidx]
+      #  
+      #except Exception as e:
+      #  status = statuses[0]
       
       length = lengths[status]
 
       guild = bot.get_guild(GUILD) 
       for ch in guild.channels:
         if ("гулаг" in ch.name):
-          res = f"Заключённый <@!{mem_id}>! Ваше заключение составит {words[status]}. Узнать свой протокол можно с помощью команды « !начальник »"
+          res = f"Заключённый <@!{mem_id}>! Ваше заключение составит {words[status]}. \nУзнать свой протокол можно с помощью команды « !начальник »"
           await ch.send(res)
 
-      sql = f"REPLACE INTO prisoners(ID, Protocol, Status) VALUES(\"{mem.id}\", \"{protocol}\", \"{status}\")"
+      sql = f"REPLACE INTO prisoners(ID, Protocol, Status, Counter) VALUES(\"{mem.id}\", \"{protocol}\", \"{status}\", \"{imprisonment_cnt}\")"
       try:
         cursor.execute(sql)
         db.commit()
@@ -779,12 +1032,9 @@ def convert_brief(message):
 
 #bot.remove_command("help")
 #
-#@bot.command(
-#  name='help',
-#  brief='Shows this message KKona',
-#)
+#@bot.command(name='help')
 #async def help(ctx):
-#  desc =  ""
+#  desc = ""
 #  for cmd in list(bot.commands):
 #    desc += f'\t{cmd.name}\t\t\t{cmd.brief}\n' 
 #  embed=discord.Embed(title="Как управляться с Манкуртом", description=desc, color=0x8c8c8c)
@@ -2244,6 +2494,24 @@ async def blacklist_donos(ctx, donos_id):
     await ctx.send(f"<@!{ctx.author.id}>, такого доноса нет!")
     return
 
+def get_description(id_to_search):
+
+    db, cursor = get_db_cursor()
+
+    select = f"SELECT * from confessions WHERE ID={id_to_search};"
+
+    try:
+      cursor.execute(select)
+      confession = cursor.fetchone()['Confession']
+      return confession
+      
+    except Exception as e:
+      print(e)
+      db.rollback()
+    db.close()
+
+    return False
+
 @bot.command(name="кто")
 async def who(ctx, mem):
     
@@ -2266,20 +2534,11 @@ async def who(ctx, mem):
     #  return
 
     db, cursor = get_db_cursor()
-
-    select = f"SELECT * from confessions WHERE ID={id_to_search};"
-
-    try:
-      cursor.execute(select)
-      confession = cursor.fetchone()['Confession']
+    confession = get_description(id_to_search)
+    if (confession):
       await ctx.send(f"<@!{id_author}>, вот что {mem.name} говорит о себе: \n\n\t*{confession}*")
-      
-    except Exception as e:
-      print(e)
-      db.rollback()
+    else:
       await ctx.send(f"<@!{id_author}>, пока нам ничего не известно о {mem.name}.")
-      
-    db.close()
 
 @bot.command(name='resetforgetforgetforget9832164382746')
 async def confesss(ctx):
@@ -2353,21 +2612,27 @@ async def confess(ctx, *, args=None):
     #data = data.replace("\"", "\\\"")
 
     row = get_db_row("confessions", iid)
-    if row:
-      #points = row["Points"]
+    
 
-      if (row["Points"] is None):
-        data = {}
-        
-      else:
-        data = json.loads(row["Points"])
-        #data[f"{id_author}"] = points
+    #if row:
+    #  #points = row["Points"]
 
-      data = json.dumps(data)
-      data = data.replace("\"", "\\\"")
+    #  if (row["Points"] is None):
+    #    data = {}
+    #    
+    #  else:
+    #    data = json.loads(row["Points"])
+    #    #data[f"{id_author}"] = points
 
-    else:
-      data = {}
+    #  data = json.dumps(data)
+    #  data = data.replace("\"", "\\\"")
+
+    #else:
+    #  data = {}
+    
+    # Not doing the above method anymore when old marks for the older confession are preserved
+    # Resetting the marks now so that they are consisten with entries in the unmarked_confession table
+    data = {}
 
     db, cursor = get_db_cursor()
 
