ef15b5da57d84a5eb22414514b090b092aa663b6
LeonKgz <leonkhalmetov@gmail.com>
2022-01-23 00:40:27 +0000

    breaking up the bot into cogs

---
+++ voice.py
@@ -0,0 +1,189 @@
+#! /usr/bin/python
+# vim: set fileencoding=utf-8:
+# coding=utf-8
+
+import asyncio
+import os
+import discord
+from discord.ext import commands, tasks
+from discord.utils import get
+import pymysql.cursors
+import json
+import random
+import re
+import requests
+import datetime
+from discord.ext.commands import Bot
+#from discord.ext import timers
+from discord import FFmpegPCMAudio
+from youtube_dl import YoutubeDL
+from requests import get
+from utils import *
+from discord import FFmpegPCMAudio
+from discord.ext import commands
+from discord.utils import get
+# test comment
+
+class Voice(commands.Cog):
+
+  def __init__(self, bot):
+    self.bot = bot  
+
+  @commands.command(name='on')
+  async def play(self, ctx, number):
+      channel = ctx.message.author.voice.channel
+      player = await channel.connect()
+      number = int(number)
+      if (number == 0):
+        # Музыка
+        player.play(FFmpegPCMAudio('http://server.audiopedia.su:8000/music128'))
+      elif number == 1:  
+        # Старое радио
+        player.play(FFmpegPCMAudio('http://server.audiopedia.su:8000/ices128'))
+      elif number == 2:  
+        # Детское радио
+        player.play(FFmpegPCMAudio('http://server.audiopedia.su:8000/detskoe128'))
+
+  #Get videos from links or from youtube search
+  def search(self, arg):
+      with YoutubeDL({'format': 'bestaudio', 'noplaylist':'False', 'ignoreerrors': 'True'}) as ydl:
+          try: requests.get(arg)
+          except: info = ydl.extract_info(f"ytsearch:{arg}", download=False)['entries']
+          else: info = ydl.extract_info(arg, download=False)
+
+      es = [dict(i) for i in info["entries"]]
+      ret = []
+      for i in es:
+        url = i["formats"][0]['url']
+        ret.append((i, url))
+      return ret
+      #return (info, info['formats'][0]['url'])
+
+
+  @commands.command(name='yt')
+  async def play(self, ctx, *, query):
+      #Solves a problem I'll explain later
+      FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+      
+      # query should be a link to the playlist (not ay video within the playlist!)
+  #    video, source = search(query)
+      video_sources = self.search(query)
+      channel = ctx.author.voice.channel
+      voice = await channel.connect() 
+
+      curr = 0
+      while True:
+
+        video, source = video_sources[curr]
+        curr += 1
+        
+        if (curr >= len(video_sources)):
+          curr = 0
+        
+        title = video['title']
+        await ctx.send(f"Now playing {title}.")
+
+        voice.play(FFmpegPCMAudio(source, **FFMPEG_OPTS), after=lambda e: print('done', e))
+        while(voice.is_playing()):
+          await asyncio.sleep(10)
+          continue
+
+  # @bot.command(name='31')
+  # async def kaligula1(ctx):
+  #     channel = ctx.message.author.voice.channel
+  #     player = await channel.connect()
+  #
+  #     for x in bot.voice_clients:
+  #
+  #        if (x.is_connected()):
+  #            x.stop()
+  #            while (True):
+  #                x.play(FFmpegPCMAudio('files/gong.mp3'))
+  #        else:
+  #
+  #            channel = ctx.message.author.voice.channel
+  #            player = await channel.connect()
+  #            while (True):
+  #                player.play(FFmpegPCMAudio('files/gong.mp3'))
+
+  @commands.command(name='1')
+  async def kaligula1(self, ctx):
+      channel = ctx.message.author.voice.channel
+      player = await channel.connect()
+
+      for x in bot.voice_clients:
+        if (x.is_connected()):
+            x.stop()
+            x.play(FFmpegPCMAudio('files/lakki1.mp3'))
+            while(player.is_playing()):
+                await asyncio.sleep(1)
+                continue
+            await x.disconnect()
+
+        else:
+            channel = ctx.message.author.voice.channel
+            player = await channel.connect()
+            player.play(FFmpegPCMAudio('files/lakki1.mp3'))
+
+            while(player.is_playing()):
+                await asyncio.sleep(1)
+                continue
+            await x.disconnect()
+
+  @commands.command(name='2')
+  async def kaligula2(self, ctx):
+      channel = ctx.message.author.voice.channel
+      player = await channel.connect()
+
+      for x in bot.voice_clients:
+        if (x.is_connected()):
+            x.stop()
+            x.play(FFmpegPCMAudio('files/lakki2.mp3'))
+            while(player.is_playing()):
+                await asyncio.sleep(1)
+                continue
+
+            await x.disconnect()
+
+        else:
+            channel = ctx.message.author.voice.channel
+            player = await channel.connect()
+            player.play(FFmpegPCMAudio('files/lakki2.mp3'))
+            while(player.is_playing()):
+                await asyncio.sleep(1)
+                continue
+            await x.disconnect()
+
+  @commands.command(name='3')
+  async def kaligula2(self, ctx):
+      channel = ctx.message.author.voice.channel
+      player = await channel.connect()
+
+      for x in bot.voice_clients:
+        if (x.is_connected()):
+            x.stop()
+            x.play(FFmpegPCMAudio('files/nicksex.mp3'))
+            while(player.is_playing()):
+                await asyncio.sleep(1)
+                continue
+            await x.disconnect()
+
+        else:
+            channel = ctx.message.author.voice.channel
+            player = await channel.connect()
+            player.play(FFmpegPCMAudio('files/nicksex.mp3'))
+            while(player.is_playing()):
+                await asyncio.sleep(1)
+                continue
+            await x.disconnect()
+
+  @commands.command(name='off', pass_context = True)
+  async def leavevoice(self, ctx):
+      for x in bot.voice_clients:
+          if(x.guild == ctx.message.guild):
+              return await x.disconnect()
+
+      return await ctx.send("I am not connected to any voice channel on this server!")
+
+def setup(bot):
+  bot.add_cog(Voice(bot))
