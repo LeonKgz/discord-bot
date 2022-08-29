1a898168f7a6b45dbe0502e5c4500d910d96392c
LeonKgz <leonkhalmetov@gmail.com>
2022-08-30 00:10:24 +0600

    testing pickle and docker

--- manaschi.py
+++ manaschi.py
@@ -5,7 +5,7 @@
 import discord
 from discord.ext import commands, tasks
 from voice import Voice
-from utils import status_update
+from utils import load_pickle, respond, save_pickle, status_update
 from env import *
 
 intents = discord.Intents.all()
@@ -15,6 +15,21 @@ bot = commands.Bot(intents=intents, command_prefix="!")
 async def on_ready():
   print(f'{bot.user.name} has connected to Discord!')
   await status_update(bot)
+
+@bot.command(name="write")
+async def mina(ctx, key, value):
+  dict = {}
+  dict[key] = value
+  save_pickle(dict, "testpickle")
+  await respond(ctx, "written to pickle!")
+
+@bot.command(name="read")
+async def mina(ctx, key):
+  dict = load_pickle("testpickle")
+  if key not in dict:
+    await respond(ctx, "empty!")
+    return
+  await respond(ctx, f"value of {key} is {dict[key]}")
 
 @tasks.loop(seconds=21600.0)
 async def status_update_loop():
